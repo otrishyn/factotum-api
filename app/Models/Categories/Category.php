89743ddab9f8b9eb60c562a11e3a1393dafe2c76
aletrix <trishin.alexey.victorovich@gmail.com>
2016-10-28 01:24:51 +0300

    Add category/type relations

--- app/Models/Categories/Category.php
+++ app/Models/Categories/Category.php
@@ -4,7 +4,18 @@ namespace App\Models\Categories;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Class Category
+ *
+ * @package App\Models\Categories
+ */
 class Category extends Model
 {
-    //
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function types()
+    {
+        return $this->hasMany(Type::class);
+    }
 }
--- app/Models/Categories/Type.php
+++ app/Models/Categories/Type.php
@@ -4,7 +4,18 @@ namespace App\Models\Categories;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Class Type
+ *
+ * @package App\Models\Categories
+ */
 class Type extends Model
 {
-    //
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
