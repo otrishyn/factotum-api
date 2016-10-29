693185ef73d5f32189b7ddcc4ed4d2630bd6db7a
aletrix <trishin.alexey.victorovich@gmail.com>
2016-10-30 01:19:05 +0300

    Add default transformer

---
+++ app/lib/Factotum/Transformers/DummyTransformer.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace app\lib\Factotum\Transformers;
+
+use Illuminate\Database\Eloquent\Model;
+use League\Fractal;
+
+/**
+ * Class DummyTransformer
+ *
+ * @package app\lib\Factotum\Transformers
+ */
+class DummyTransformer extends Fractal\TransformerAbstract
+{
+    /**
+     * @param Model $model
+     * @return array
+     */
+    public function transform($model)
+    {
+        return $model->toArray();
+    }
+}
