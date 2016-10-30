d184292e501fe0f15a8fda6ee92dd10bd667ea02
aletrix <trishin.alexey.victorovich@gmail.com>
2016-10-30 03:03:04 +0300

    Prepare api output

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,12 +2,33 @@
 
 namespace App\Http\Controllers;
 
+use Factotum\Helpers\OutputResponse;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 
+/**
+ * Class Controller
+ *
+ * @package App\Http\Controllers
+ */
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    /**
+     * @var \Factotum\Helpers\OutputResponse
+     */
+    protected $response;
+
+    /**
+     * Controller constructor.
+     *
+     * @param \Factotum\Helpers\OutputResponse $outputResponse
+     */
+    public function __construct(OutputResponse $outputResponse)
+    {
+        $this->response = $outputResponse;
+    }
 }
