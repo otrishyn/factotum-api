33d4044643c9245c3ee76503000474cd95539dd2
aletrix <trishin.alexey.victorovich@gmail.com>
2016-11-09 23:26:59 +0200

    Use category transformer

--- app/Http/Controllers/Category/CategoryController.php
+++ app/Http/Controllers/Category/CategoryController.php
@@ -13,63 +13,81 @@ class CategoryController extends ApiController
      * Display a listing of the resource.
      *
      * @param Request $request
-     * @return \Illuminate\Http\Response
+     * @return array
      */
     public function index(Request $request)
     {
-        return $this->respondWithCollection(Category::paginate(10), new CategoryTransformer(), 'category');
+        return $this->respondWithCollection(
+            Category::paginate($request->input('limit', $this->defaultLimit)),
+            new CategoryTransformer(),
+            'category'
+        );
     }
-
+    
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @param  \Illuminate\Http\Request $request
+     * @return array
      */
     public function store(Request $request)
     {
-        return Category::create([
-            'name'=>$request->input('name'),
-            'queue'=>Category::max('queue') + 1
-        ]);
+        return $this->respondWithItem(
+            Category::create(
+                [
+                    'name' => $request->input('name'),
+                    'queue' => Category::max('queue') + 1,
+                ]
+            ),
+            new CategoryTransformer(),
+            'category'
+        );
     }
-
+    
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @param  int $id
+     * @return array
      */
     public function show($id)
     {
-        return Category::findOrFail($id);
+        return $this->respondWithItem(
+            Category::findOrFail($id),
+            new CategoryTransformer(),
+            'category'
+        );
     }
-
+    
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
+     * @param  \Illuminate\Http\Request $request
+     * @param  int $id
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, $id)
     {
         $category = Category::findOrFail($id);
         $category->update($request->all());
-
-        return $category;
+        
+        return $this->respondWithItem(
+            $category,
+            new CategoryTransformer(),
+            'category'
+        );
     }
-
+    
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @param  int $id
+     * @return \Illuminate\Http\JsonResponse
      */
     public function destroy($id)
     {
         Category::findOrFail($id)->delete();
         
-        return $this->statusResponse(204);
+        return $this->respondWithOk();
     }
 }
