93aeb9a80004d871601b41218f7114d09159668d
aletrix <trishin.alexey.victorovich@gmail.com>
2016-12-14 01:54:20 +0200

    Add possibility to check user state

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -8,7 +8,6 @@ class Kernel extends HttpKernel
 {
     /**
      * The application's global HTTP middleware stack.
-     *
      * These middleware are run during every request to your application.
      *
      * @var array
@@ -16,7 +15,7 @@ class Kernel extends HttpKernel
     protected $middleware = [
         \Illuminate\Foundation\Http\Middleware\CheckForMaintenanceMode::class,
     ];
-
+    
     /**
      * The application's route middleware groups.
      *
@@ -31,16 +30,15 @@ class Kernel extends HttpKernel
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
-
+        
         'api' => [
             'throttle:60,1',
             'bindings',
         ],
     ];
-
+    
     /**
      * The application's route middleware.
-     *
      * These middleware may be assigned to groups or used individually.
      *
      * @var array
@@ -52,5 +50,6 @@ class Kernel extends HttpKernel
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
+        'user.status' => \App\Http\Middleware\VerifyUserState::class,
     ];
 }
